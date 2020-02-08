acd0e29e1282e5d7fbb9ede192570d2dd13a3aae
rabah babaci <4rabah@gmail.com>
2020-02-08 09:57:25 -0800

    create cardInfo component

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CardName from "./CardName";
+import CardInfo from "./CardInfo";
 
 export default function Card() {
   return (
     <div>
       <CardName />
+      <CardInfo />
       <hello>hrhhr</hello>
     </div>
   );
