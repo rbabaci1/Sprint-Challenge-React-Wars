cafc1eb6c3c6ddd1a15d031e396bc87f9de8f99f
rabah babaci <4rabah@gmail.com>
2020-02-08 10:07:19 -0800

    set initial state data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,12 +6,7 @@ import Card from "./components/Card";
 // https://swapi.co/api/people/
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [starWarsData, setStarWarsData] = useState({});
 
   return (
     <div className="App">
