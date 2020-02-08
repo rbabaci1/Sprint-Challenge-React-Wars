c55e92474fe43c81639646d65ede784752ede475
rabah babaci <4rabah@gmail.com>
2020-02-08 09:55:45 -0800

    add card and cardName components

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CardName from "./CardName";
+
+export default function Card() {
+  return (
+    <div>
+      <CardName />
+      <hello>hrhhr</hello>
+    </div>
+  );
+}
---
+++ starwars/src/components/CardName.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CardName() {
+  return (
+    <div>
+      <h1>Name</h1>
+    </div>
+  );
+}
