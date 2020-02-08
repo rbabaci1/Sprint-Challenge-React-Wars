441aba676be0b63c12728718c2cd8d6fc9463b99
rabah babaci <4rabah@gmail.com>
2020-02-08 10:53:23 -0800

    style name section

--- starwars/src/components/CardName.js
+++ starwars/src/components/CardName.js
@@ -3,12 +3,27 @@ import styled from "styled-components";
 
 const NameDiv = styled.div`
   border-bottom: 2px solid gray;
+
+  h1 {
+    margin: 10px 0;
+    font-size: 25px;
+    font-family: Monaco;
+    color: #555050;
+
+    span {
+      font-weight: bold;
+      color: black;
+    }
+  }
 `;
 
 export default function CardName({ charName }) {
   return (
     <NameDiv>
-      <h1>{charName}</h1>
+      <h1>
+        <span>Name: </span>
+        {charName}
+      </h1>
     </NameDiv>
   );
 }
